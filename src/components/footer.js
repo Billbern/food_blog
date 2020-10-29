d3d29598ad05f90866cbf747ba386775b1e77970
Billbern <denis.abaidoo@gmail.com>
2020-10-29 10:50:34 +0000

    added footer component

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>Copyright &copy; Simple Food 2020</span>
+      <div className="follow_us">
+        <i></i>
+        <i></i>
+        <i></i>
+      </div>
+    </div>
+  );
+}
